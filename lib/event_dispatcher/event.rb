bfd01cc506285aee55b2d4b3d2617e05da1b9acb
Amine Asli <asli.amine@gmail.com>
2013-12-18 02:02:22 +0000

    Update event.rb

--- lib/event_dispatcher/event.rb
+++ lib/event_dispatcher/event.rb
@@ -1,7 +1,7 @@
 module EventDispatcher
    module Event
       attr_accessor :stop_propagation
-      # A listener can prevent any other listeners from being called. 
+      # A listener can prevent any other listeners from being called by setting stop_propagation to true.
       def initialize
          @stop_propagation = false
       end
